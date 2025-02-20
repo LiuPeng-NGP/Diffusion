135c36ed5dd5c2dd0582893848720a60be904e28
LiuPeng-NGP <liupeng_ngp@outlook.com>
2025-02-20 19:02:02 +0800

    DDPM and EDM

---
+++ Class_conditional_DDPM/ddpm.py
@@ -0,0 +1,67 @@
+import torch
+from tqdm import tqdm
+
+class DDPM():
+    def __init__(self, model, T=1000):
+        self.model = model
+        
+        self.beta_start = 0.0001
+        self.beta_end = 0.02
+        self.T = T
+
+        # Correct concatenation of beta_t
+        self.betas = torch.cat([torch.tensor([0.0]), torch.linspace(self.beta_start, self.beta_end, self.T)])
+        self.sqrt_betas = torch.sqrt(self.betas)
+        self.alphas = 1 - self.betas
+        self.log_alphas = torch.log(self.alphas)
+        self.alphas_bar = torch.cumsum(self.log_alphas, dim=0).exp()
+
+        # Training Algorithm 1
+        self.sqrt_alphas_bar = torch.sqrt(self.alphas_bar) 
+        self.sqrt_one_minus_alphas_bar = torch.sqrt(1 - self.alphas_bar)
+
+        # Sampling Algorithm 2
+        self.one_over_sqrt_alphas = 1 / torch.sqrt(self.alphas)
+        self.one_minus_alpha_over_sqrt_one_minus_alpha_bar = (1 - self.alphas) / self.sqrt_one_minus_alphas_bar 
+
+    def perturb(self, x):
+        t = torch.randint(1, self.T + 1, (x.shape[0], )).to(x.device)
+        noise = torch.randn_like(x)
+        x_noised = (self.sqrt_alphas_bar.to(x.device)[t, None, None, None] * x +
+                self.sqrt_one_minus_alphas_bar.to(x.device)[t, None, None, None] * noise)
+        return x_noised, t, noise
+
+    def loss(self, x, class_labels=None):
+        x_noised, t, noise = self.perturb(x)
+        loss = (noise - self.model(x_noised, t/self.T, class_labels))**2 
+        return loss.mean()
+
+    def sample(self, latents, class_labels=None, no_tqdm=False):
+        size = latents.shape
+        for i in tqdm(range(self.T, 0, -1), disable=no_tqdm):
+            t = torch.tensor([i / self.T]).to(latents.device).repeat(latents.shape[0])
+            z = torch.randn(*size).to(latents.device) if i > 1 else 0
+            eps, _ = self.pred_eps_(latents, t, class_labels, self.alphas_bar[i])
+
+            # Algorithm 2
+            mean = self.one_over_sqrt_alphas[i] * (latents - self.one_minus_alpha_over_sqrt_one_minus_alpha_bar[i] * eps)
+            variance = self.sqrt_betas[i] # Let variance sigma = sqrt_beta
+            latents = mean + variance * z
+
+        x_0 = latents
+        return x_0
+
+    def pred_eps_(self, x, t, class_labels, alpha, clip_x=True):
+        def pred_eps_from_x0(x0):
+            return (x - x0 * alpha.sqrt()) / (1 - alpha).sqrt()
+
+        def pred_x0_from_eps(eps):
+            return (x - (1 - alpha).sqrt() * eps) / alpha.sqrt()
+        
+        eps = self.model(x, t, class_labels).float()
+        denoised = pred_x0_from_eps(eps)
+
+        if clip_x:
+            denoised = torch.clip(denoised, -1., 1.)
+            eps = pred_eps_from_x0(denoised)
+        return eps, denoised
--- README.md
+++ README.md
@@ -1,17 +1,52 @@
 # Diffusion
 My replication for diffusion model.
 
+# Environment
+conda env create -f environment.yml
+conda activate diffusion
+
+# Data
+run the code in extract_data.ipynb to download CIFAR-10 data.
+
 # Unconditional DDPM
+cd unconditional_DDPM
+## Train
+torchrun --nnodes=1 --nproc_per_node=2 train.py --config config.yaml --use_amp
+## Sample
+torchrun --nnodes=1 --nproc_per_node=2 sample.py --config config.yaml --use_amp
+## Evaluation
+python3 -m pytorch_fid unconditional_DDPM/2000_unconditional_DDPM/EMAgenerated_ep1999_ddpm_steps1000/pngs data/cifar10-pngs
+FID:  3.2444050218018106
 
 
 # Class-condiitonal DDPM
+cd Class_conditional_DDPM
+## Train
+torchrun --nnodes=1 --nproc_per_node=2 --rdzv_endpoint=localhost:55000 train.py --config config.yaml --use_amp
+## Sample
+torchrun --nnodes=1 --nproc_per_node=2 --rdzv_endpoint=localhost:55500 sample.py --config config.yaml --use_amp
+## Evaluation
+python3 -m pytorch_fid Class_conditional_DDPM/2000_conditional_DDPM/EMAgenerated_ep1999_ddpm_steps1000/pngs data/cifar10-pngs
+FID:  2.9473849136342665
 
-
-# Unconditional DDPM
-
+# Unconditional EDM
+cd unconditional_EDM
+## Train
+torchrun --nnodes=1 --nproc_per_node=2 --rdzv_endpoint=localhost:55001 train.py --config config.yaml --use_amp
+## Sample
+torchrun --nnodes=1 --nproc_per_node=2 --rdzv_endpoint=localhost:55501 sample.py --config config.yaml --use_amp
+## Evaluation
+python3 -m pytorch_fid unconditional_EDM/1200_unconditional_EDM/EMAgenerated_ep1199_edm_steps18/pngs data/cifar10-pngs
+FID:  4.234571648031874
 
 # Class-condiitonal EDM
-
+## Train
+torchrun --nnodes=1 --nproc_per_node=2 --rdzv_endpoint=localhost:55002 train.py --config config.yaml --use_amp
+## Sample
+torchrun --nnodes=1 --nproc_per_node=2 --rdzv_endpoint=localhost:55502 sample.py --config config.yaml --use_amp
+## Evaluation
+python3 -m pytorch_fid Class_conditional_EDM/1200_conditional_EDM/EMAgenerated_ep1199_edm_steps18/pngs data/cifar10-pngs
+FID:  3.707334678054849
 
 # Unconditional Consistency Model
 
